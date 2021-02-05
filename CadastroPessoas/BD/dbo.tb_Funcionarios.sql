f9371f383a87bd7beb59488fb1cbc60c541b60b9
Elias Abreu do Couto <73173398+helyascouto@users.noreply.github.com>
2021-02-05 12:32:49 -0300

    add

---
+++ CadastroPessoas/BD/dbo.tb_Funcionarios.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[tb_Funcionarios] (
+    [Id]         INT           IDENTITY (1, 1) NOT NULL,
+    [nome]       NVARCHAR (50) NOT NULL,
+    [sexo]       CHAR (10)     NULL,
+    [TELEFONE]   INT    NULL,
+    [SALARIO]    REAL          NULL,
+    [EMAIL]      NCHAR (30)    NULL,
+    [NASCIMENTO] DATETIME      NULL,
+    [CIDADE]     NCHAR (20)    NULL,
+    [ESTADO]     NCHAR (20)    NULL,
+    [CEP]        INT           NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
