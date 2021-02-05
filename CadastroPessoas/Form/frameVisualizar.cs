a3c7c92d57d4a9a21375bfab8f25342283f1e190
Elias Abreu do Couto <73173398+helyascouto@users.noreply.github.com>
2021-02-05 12:44:07 -0300

    correção

--- CadastroPessoas/Form/frameVisualizar.cs
+++ CadastroPessoas/Form/frameVisualizar.cs
@@ -24,7 +24,7 @@ namespace CadastroPessoas
 
             {
 
-                conexao = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\BUTFIRE\Desktop\CadastroPessoas\CadastroPessoas\BD\DB_SistemaCadastro.mdf;Integrated Security=True;Connect Timeout=30");
+                conexao = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\BUTFIRE\crud\CadastroPessoas\BD\DB_SistemaCadastro.mdf;Integrated Security=True;Connect Timeout=30");
                 String comandoSql = "SELECT * FROM TB_FUNCIONARIOS";
                 SqlDataAdapter da = new SqlDataAdapter(comandoSql, conexao);
                 DataSet ds = new DataSet();
